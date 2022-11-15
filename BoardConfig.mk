213232dd50eefcf77599d6508ca39b2590367f89
SathamHussainM <sathamhussain.m11@gmail.com>
2022-11-15 15:38:08 +0000

    r5x: Import blobs & libs from stock C77
    Signed-off-by: SathamHussainM <sathamhussain.m11@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,9 +48,6 @@ TARGET_USES_UEFI := true
 # Assert
 TARGET_OTA_ASSERT_DEVICE := RMX1911,RMX1925,RMX2030,RMX1927,r5x
 
-# fstab
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery.fstab
-
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1 androidboot.usbcontroller=a600000.dwc3 firmware_class.path=/vendor/firmware_mnt/image earlycon=msm_geni_serial,0x880000 loop.max_part=7 cgroup.memory=nokmem,nosocket androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
@@ -100,6 +97,7 @@ BOARD_SUPPRESS_SECURE_ERASE := true
 TARGET_NO_RECOVERY := false
 
 # Crypto
+TW_INCLUDE_FBE := true
 TW_INCLUDE_CRYPTO := true
 BOARD_USES_QCOM_FBE_DECRYPTION := true
 TW_USE_FSCRYPT_POLICY := 1
@@ -111,7 +109,7 @@ PLATFORM_VERSION := 127
 PLATFORM_VERSION_LAST_STABLE := $(PLATFORM_VERSION)
 
 # Fstab
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery.fstab
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
 
 # TWRP specific build flags
 RECOVERY_SDCARD_ON_DATA := true
